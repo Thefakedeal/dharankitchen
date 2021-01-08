98d66804137bb3ef0d6781fb8b552f68afd2438d
Samir Shrestha <theonlysamir@gmail.com>
2021-01-08 12:05:44 +0545

    Events Model Added

---
+++ app/Models/EventImage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EventImage extends Model
+{
+    use HasFactory;
+}
