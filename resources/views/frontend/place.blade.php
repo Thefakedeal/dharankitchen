52c6b038898d4af901f848b55570084eeab14618
Sajal Shrestha <codeitsajal@gmail.com>
2021-01-22 14:42:29 +0545

    minor changes in place

--- resources/views/frontend/place.blade.php
+++ resources/views/frontend/place.blade.php
@@ -1,18 +1,23 @@
 @extends('frontend.templates.app')
 @section('main')    
-<div class="container">
-        <div class="row">
-            <div class="col">
-                <img src="{{ $place->image }}" alt="..." class="  img-fluid object-cover" height='30vh'>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="py-2">{!! $place->description !!}</p>
+<div class="container py-4">
+        <h1 class="text-center display-4">Tourist Attraction</h1>
+        <h5 class="text-center fs-4 fw-light">Places near dharan</h5>
+        <hr>
+        <div class="row py-4">
+          <div class="col-md-4">
+              <img src="{{ $place->image }}" alt="" class="img-thumbnail">
+          </div>
+            <div class="col-md-8">
+                <h1 class="display-6 fs-3">{{ $place->name }}</h1>
+                <p class="py-2 fw-light">{!! $place->description !!}</p>
             </div> 
         </div>
+
+        <hr>
         <div class="row">
-            <h2 class="text-center">Other Places</h2>
+        <h1 class="text-center display-6 fs-3">Similar Places</h1>
+            
             @foreach ($places as $place)
             <div class="col-md-3 py-2">
               <div class="card">
